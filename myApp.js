9905fb01f756c9adac0f698cae1b5f8c18f3485d
Glitch (smart-radio) <none>
2018-12-27 02:22:45 +0000

    🕵🍽 Updated with Glitch

--- myApp.js
+++ myApp.js
@@ -12,7 +12,7 @@ var app = express();              // Do Not Edit
 ********************/ 
 
 /** 1) Install and require `helmet` */
-
+var helmet = require('helmet');
 // [Helmet](https://github.com/helmetjs/helmet) helps you secure your
 // Express apps by setting various HTTP headers.
 // Install the package, then require it.
@@ -30,8 +30,8 @@ var app = express();              // Do Not Edit
 // people off. e.g. `helmet.hidePoweredBy({ setTo: 'PHP 4.2.0' })`
 
 // Use `helmet.hidePoweredBy()``
-
-
+app.use(helmet());
+app.use(helmet.hidePoweredBy());
 
 /** 3) Mitigate the risk of clickjacking - `helmet.frameguard()` */
 
@@ -45,7 +45,7 @@ var app = express();              // Do Not Edit
 
 // We don't need our app to be framed, so you should use `helmet.frameguard()`
 // passing to it the configuration object `{action: 'deny'}`
-
+app.use(helmet.frameguard({action: 'deny'}));
  
 
 /** 4) Mitigate the risk of XSS - `helmet.xssFilter()` */
@@ -68,7 +68,7 @@ var app = express();              // Do Not Edit
 // It still has limited support.
 
 // Use `helmet.xssFilter()`
-
+app.use(helmet.xssFilter());
 
 
 /** 5) Avoid inferring the response MIME type - `helmet.noSniff()` */
@@ -81,7 +81,7 @@ var app = express();              // Do Not Edit
 // instructing the browser to not bypass the provided `Content-Type`.
 
 // Use `helmet.noSniff()`
-
+app.use(helmet.noSniff())
 
 
 /** 6) Prevent IE from opening *untrusted* HTML - `helmet.ieNoOpen()` */
@@ -94,7 +94,7 @@ var app = express();              // Do Not Edit
 // to prevent IE users from executing downloads in the *trusted* site's context.
 
 // Use `helmet.ieNoOpen()`
-
+app.use(helmet.ieNoOpen())
 
 
 /**  7) Ask browsers to access your site via HTTPS only - `helmet.hsts()` */
@@ -115,6 +115,7 @@ var app = express();              // Do Not Edit
 
 var ninetyDaysInMilliseconds = 90*24*60*60*1000;
 
+app.use(helmet.hsts({maxAge: ninetyDaysInMilliseconds}))
 
 //**Note**:
 // Configuring HTTPS on a custom website requires the acquisition of a domain,
@@ -133,7 +134,7 @@ var ninetyDaysInMilliseconds = 90*24*60*60*1000;
 
 // Use `helmet.dnsPrefetchControl()`
 
-
+app.use(helmet.dnsPrefetchControl());
 
 /** 9) Disable Client-Side Caching - `helmet.noCache()` */
 
@@ -144,7 +145,7 @@ var ninetyDaysInMilliseconds = 90*24*60*60*1000;
 // use this option only when there is a real need.
 
 // Use helmet.noCache()
-
+app.use(helmet.noCache());
 
 
 /** 10) Content Security Policy - `helmet.contentSecurityPolicy()` */
@@ -175,7 +176,7 @@ var ninetyDaysInMilliseconds = 90*24*60*60*1000;
 // in the `"'self'"` keyword, the single quotes are part of the keyword itself, 
 // so it needs to be enclosed in **double quotes** to be working.
 
-
+app.use(helmet.contentSecurityPolicy({directives:{defaultSrc:["'self'"], scriptSrc: ["'self'", 'trusted-cdn.com']}}));
 
 /** TIP: */ 
 
